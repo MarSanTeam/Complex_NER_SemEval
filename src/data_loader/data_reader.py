09a5ec27efb08042a297acf9e3ace643df04f141
ehsan.tavan <ehsan.tavan@partsoftware.com>
2022-09-08 10:09:22 +0430

    remove read_pickle function

--- src/data_loader/data_reader.py
+++ src/data_loader/data_reader.py
@@ -25,20 +25,3 @@ def read_text(path: str) -> list:
     with open(path, "r", encoding="utf8") as file:
         data = file.readlines()
     return data
-
-
-def read_pickle(path: str) -> list or dict:
-    """
-    function to read pickle file
-
-    Args:
-        path: path of pickle file
-
-    Returns:
-        data in pickle file
-
-    """
-
-    with open(path, "rb") as file:
-        data = pickle.load(file)
-    return data
